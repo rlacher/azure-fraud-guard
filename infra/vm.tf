bd98bf971aceba7117f2df335aff22c94cdc9a90
René Lacher <renemlacher@gmail.com>
2025-06-26 23:23:18 +0200

    fix: Ensure Kafka topic readiness and broker stability on Azure VM
    - Upscales VM to `Standard_B1ms` to prevent out-of-memory errors during Kafka startup.
    - Adds init service to explicitly create `transactions` topic before producer runs.

--- infra/vm.tf
+++ infra/vm.tf
@@ -6,7 +6,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   location                        = azurerm_resource_group.rg.location
   resource_group_name             = azurerm_resource_group.rg.name
   network_interface_ids           = [azurerm_network_interface.nic.id]
-  size                            = "Standard_B1s"
+  size                            = "Standard_B1ms"
   admin_username                  = var.admin_username
   admin_password                  = var.admin_password
   disable_password_authentication = false
